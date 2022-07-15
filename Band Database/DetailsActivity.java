12111acb7af45a3d1d1e11ab176e380bead9dc0c
Christopher Shaw <C.I.Shaw@outlook.com>
2022-07-14 17:12:34 -0700

    Initial Push

---
+++ Band Database/DetailsActivity.java
@@ -0,0 +1,33 @@
+/*
+Chris Shaw
+The Band Database
+3/18/20
+ */
+package edu.lwtech.thebanddatabase;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+
+public class DetailsActivity extends AppCompatActivity {
+
+    public static String EXTRA_BAND_ID = "bandId";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_details);
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        Fragment fragment = fragmentManager.findFragmentById(R.id.details_fragment_container);
+
+        if (fragment == null) {
+            int bandId = getIntent().getIntExtra(EXTRA_BAND_ID, 1);
+            fragment = DetailsFragment.newInstance(bandId);
+            fragmentManager.beginTransaction()
+                    .add(R.id.details_fragment_container, fragment)
+                    .commit();
+        }
+    }
+}
